c4d30023e6eabcc7be7a0803308468c91c4df436
leejeawoo <novaljw@gmail.com>
2021-01-04 08:43:40 +0900

    Member_Idao.java 작성

---
+++ project_test/src/main/java/com/min/edu/model/Member_IDao.java
@@ -0,0 +1,21 @@
+package com.min.edu.model;
+
+import java.util.List;
+import java.util.Map;
+
+import com.min.edu.dto.Member_Dto;
+
+public interface Member_IDao {
+
+	// 전체회원 조회
+	public List<Member_Dto> memList();
+	
+	// 회원가입
+	public boolean signupMember(Member_Dto dto);
+	
+	// 아이디 중복 체크 
+	public boolean idDuplicateCheck(String id);
+	
+	// 로그인
+	public Member_Dto loginMember(Map<String, Object> map);
+}
